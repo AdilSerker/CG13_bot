b4c74f4a8d30a3acce4ec35627bab4b9f2a6cfd6
Adil <adilserkerov@gmail.com>
2020-06-17 17:44:33 +0300

    save users

--- src/application/bot-controllers/OnListeners.ts
+++ src/application/bot-controllers/OnListeners.ts
@@ -1,11 +1,20 @@
-import { UpdateType, MessageSubTypes } from "telegraf/typings/telegram-types";
-import { Middleware } from "telegraf";
+import { userRepository } from './../../infrastructure/repositories/UserRepository';
+
 import { TelegrafContext } from "telegraf/typings/context";
 import { OnListener } from "../../types";
 
 class Listners {
     static async onMessage(ctx: TelegrafContext) {
-        
+
+
+        const user_from = ctx.update.message.from;
+
+
+        const user = await userRepository.get(user_from.id);
+
+        if (!user) {
+            await userRepository.save(user_from);
+        }
     }
 
     static async onSticker(ctx: TelegrafContext) {
