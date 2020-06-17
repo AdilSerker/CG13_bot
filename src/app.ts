9c4d26950121d7bedec47b915a2be23eb17ec544
Adil <adilserkerov@gmail.com>
2020-06-17 07:00:22 +0300

    test commands

--- src/app.ts
+++ src/app.ts
@@ -24,6 +24,9 @@ bot.start((ctx) => ctx.reply('Welcome'));
 bot.help((ctx) => ctx.reply('Send me a sticker'));
 bot.on('sticker', (ctx) => ctx.reply('👍'));
 bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+bot.command('oldschool', (ctx) => ctx.reply('Hello'));
+bot.command('modern', ({ reply }) => reply('Yo'));
+bot.command('hipster', ({ reply }) => reply('λ'));
 
 async function startServer() {
 
