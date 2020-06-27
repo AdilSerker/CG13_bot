75688a76fe9db4ed6889892162d0514e8765b47e
Adil <adilserkerov@gmail.com>
2020-06-28 01:06:17 +0300

    off spam

--- src/components/sheduler/sheduler.ts
+++ src/components/sheduler/sheduler.ts
@@ -6,10 +6,10 @@ import { artStationService } from '../../infrastructure/services/ArtStationServi
 
 cron.schedule('* 9,21 * * *', async () => {
     const work = await artStationService.getRandomWork();
-    await bot.telegram.sendPhoto('-1001361040825',{
-        url: work.cover.small_square_url,
-        filename: 'preview'
-    }, {
-        caption: work.title + '\n' + work.permalink
-    });
+    // await bot.telegram.sendPhoto('-1001361040825',{
+    //     url: work.cover.small_square_url,
+    //     filename: 'preview'
+    // }, {
+    //     caption: work.title + '\n' + work.permalink
+    // });
 });
