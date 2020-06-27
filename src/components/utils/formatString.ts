ed11ddd78724226ea3d05ab8a1df6e97424ce90e
Adil <adilserkerov@gmail.com>
2020-06-27 14:30:51 +0300

    debug

--- src/components/utils/formatString.ts
+++ src/components/utils/formatString.ts
@@ -1,14 +1,19 @@
 export function formatTimeStringToSeconds(timeString: string): number {
+    console.log('IN TIME_STRING', timeString)
     const [min, sec, ms] = timeString.split(':');
 
+    console.log('OUT IN SECONDS', Number(min) * 60 + Number(sec) + Number(ms) / 1000);
     return Number(min) * 60 + Number(sec) + Number(ms) / 1000;
 }
 
 export function formatSecondsToTimeString(seconds: number): string {
+    console.log('IN SECONDS');
     const min = Math.floor(seconds/60);
+    console.log('MIN', min);
     const sec = Math.floor(seconds%60);
-    console.log(Math.floor(seconds%60).toString());
+    console.log('SEC', Math.floor(seconds%60));
     const ms = Math.floor(seconds%60).toString().split(',')[1].slice(0,2);
+    console.log('MS', Math.floor(seconds%60).toString());
 
     return `${min}:${sec}:${ms}`;
 }
